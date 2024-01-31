1501bb2eb51b2d678d4ae9aa6085259c04b5cef8
kurasita you <n23012@std.it-college.ac.jp>
2024-01-31 03:54:19 +0000

    5.5 fin
    aboutページに画像を表示

--- pages/about.js
+++ pages/about.js
@@ -7,11 +7,23 @@ import {
   TwoColumnMain,
   TwoColumnSidebar
 } from 'components/two-column'
+import Images from 'next/image'
+import eyecatch from 'images/about.jpg'
 const About = () => {
   const props2 = { title: 'About', subtitle: 'About development activities' }
   return (
     <Container>
       <Hero {...props2} />
+      <figure>
+        <Image
+          src={eyecatch}
+          alt=''
+          layout='responsive'
+          sizes='(min-width: 1152px) 1152px,100vm'
+          priority
+          placeholder='blur'
+        />
+      </figure>
       <TwoColumn>
         <TwoColumnMain>
           <PostBody>
