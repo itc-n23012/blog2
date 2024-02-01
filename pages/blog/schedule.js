977f927f260e8d729e4c7ec42c27e06708a9a20c
kurasita you <n23012@std.it-college.ac.jp>
2024-02-01 06:38:22 +0000

    8.4 fin
    記事データの表示

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,6 +1,13 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
+import PostBody from 'components/post-body'
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar
+} from 'components/two-column'
+import ConvertBody from 'components/convert-body'
 import Image from 'next/image'
 
 const Schedule = props => {
@@ -23,6 +30,14 @@ const Schedule = props => {
             priority
           />
         </figure>
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={props.content} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar></TwoColumnSidebar>
+        </TwoColumn>
       </article>
     </Container>
   )
